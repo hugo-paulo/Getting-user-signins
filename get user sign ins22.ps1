89cceba59ce1eac7d875f5b5244ef0688c6f2589
hugo-paulo <47899992+hugo-paulo@users.noreply.github.com>
2023-01-01 09:43:24 +0000

    Add files via upload

---
+++ get user sign ins22.ps1
@@ -0,0 +1,10 @@
+﻿Connect-MgGraph
+
+
+Select-MgProfile -Name "beta"
+
+$myusr= Get-MgUser -UserId "<user id>" -Property SignInActivity, DisplayName, Id  |  Select-Object -Property DisplayName, Id  -ExpandProperty SignInActivity 
+
+$myusr.DisplayName
+$myusr.LastSignInDateTime
+$myusr.Id
